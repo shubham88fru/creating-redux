6aa2ad6a55bdba09cec074596d1ea554b8a79acb
shubham88fru <shubham88fru@yahoo.com>
2024-05-03 19:07:45 -0700

    can subscribe

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ function createStore() {
   // 3. Listen to changes on the state.
   // 4. Update the state.
 
+  // 1. The state.
   let state;
+  let listeners = [];
 
+  // 2. Get teh state.
   const getState = () => state;
+
+  // 3. Listen to changes on the state.
+  const subscribe = (listener) => {
+    listeners.push(listener);
+
+    //to be used by client to unsuscribe.
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
 
   return {
     getState,
+    subscribe,
   };
 }
+
+const store = createStore();
