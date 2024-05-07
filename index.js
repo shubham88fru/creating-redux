ea9e3d63ef75d33e4cbd44fbf6144642c9b6bd53
shubham88fru <shubham88fru@yahoo.com>
2024-05-07 16:10:21 -0700

    action creators

--- index.js
+++ index.js
@@ -44,6 +44,43 @@ const REMOVE_TODO = "REMOVE_TODO";
 const TOGGLE_TODO = "TOGGLE_TODO";
 const ADD_GOAL = "ADD_GOAL";
 const REMOVE_GOAL = "REMOVE_GOAL";
+
+//Action creators.
+function addTodoAction(todo) {
+  return {
+    type: ADD_TODO,
+    todo,
+  };
+}
+
+function removeTodoAction(id) {
+  return {
+    type: REMOVE_TODO,
+    id,
+  };
+}
+
+function toggleTodoAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id,
+  };
+}
+
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal,
+  };
+}
+
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id,
+  };
+}
+
 /**
  * Characteristics of a pure function -
  * 1. They always return the same result if the same arguments are passed to them.
