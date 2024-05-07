df1457cfff03061821b90dc03bf597cd8dd55d95
shubham88fru <shubham88fru@yahoo.com>
2024-05-06 19:33:19 -0700

    cleanup

--- index.js
+++ index.js
@@ -1,52 +1,4 @@
-/**
- * Characteristics of a pure function -
- * 1. They always return the same result if the same arguments are passed to them.
- * 2. They depend only on the argument passed into them.
- * 3. Never product any side effects (Should have no interaction with the outside world.)
- */
-//Reducer function (Must be pure function)
-//Created the user of the library, thats why
-//not included in the createStore().
-
-//Todos reducer - updates and return todo part of the state.
-function todos(state = [], action) {
-  switch (action.type) {
-    case "ADD_TODO":
-      return state.concat([action.todo]); //not push! concat returns a new array.
-    case "REMOVE_TODO":
-      return state.filter((todo) => todo.id !== action.id);
-    case "TOGGLE_TOOD":
-      return state.map((todo) =>
-        todo.id !== action.id
-          ? todo
-          : Object.assign({}, todo, { complete: !todo.complete })
-      );
-    default:
-      return state;
-  }
-}
-
-//Goals reducer. Updates and returns goals part of the state.
-function goals(state = [], action) {
-  switch (action.type) {
-    case "ADD_GOAL":
-      return state.concat([action.goal]);
-    case "REMOVE_GOAL":
-      return state.filter((goal) => goal.id !== action.id);
-    default:
-      return state;
-  }
-}
-
-//Combines all state in the app.
-//The root reducer.
-function app(state = {}, action) {
-  return {
-    todos: todos(state.todos, action), //todos state
-    goals: goals(state.goals, action), //goals state
-  };
-}
-
+////////////////////////LIBRARY CODE//////////////////
 function createStore(reducer) {
   // The store should have four parts
   // 1. The state.
@@ -83,5 +35,62 @@ function createStore(reducer) {
     dispatch,
   };
 }
+////////////////////////LIBRARY CODE//////////////////
+
+////////////////////////APP CODE//////////////////////
+
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+/**
+ * Characteristics of a pure function -
+ * 1. They always return the same result if the same arguments are passed to them.
+ * 2. They depend only on the argument passed into them.
+ * 3. Never product any side effects (Should have no interaction with the outside world.)
+ */
+//Reducer function (Must be pure function)
+//Created the user of the library, thats why
+//not included in the createStore().
+//Todos reducer - updates and return todo part of the state.
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]); //not push! concat returns a new array.
+    case REMOVE_TODO:
+      return state.filter((todo) => todo.id !== action.id);
+    case TOGGLE_TODO:
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+//Goals reducer. Updates and returns goals part of the state.
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+//Combines all state in the app.
+//The root reducer.
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action), //todos state
+    goals: goals(state.goals, action), //goals state
+  };
+}
 
 const store = createStore(app);
+////////////////////////APP CODE//////////////////////
